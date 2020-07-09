67365c4dc4e7b8d7c499107438c04464f2dd363a
Mohnish Goyal <mohnishgoyal26@gmail.com>
2020-07-09 15:43:05 +0530

    first commit

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:priority_list/screens/home/home.dart';
+import 'package:priority_list/screens/authenticate/authenticate.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Authenticate();
+  }
+}
